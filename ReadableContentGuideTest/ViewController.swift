4dcfa2d5571fa1ec1c35c4fe05a6d13866609f36
0eta0 <eta.develop@gmail.com>
2020-08-13 11:56:05 +0900

    Initial

---
+++ ReadableContentGuideTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReadableContentGuideTest
+//
+//  Created by eita.yamaguchi on 2020/08/13.
+//  Copyright © 2020 Eita Yamaguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
